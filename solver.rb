5c340d62749ae808c5ec2ef434ee2ce3567b7ad8
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-10-21 16:36:44 -0300

    Return value pass the test

--- solver.rb
+++ solver.rb
@@ -1,2 +1,11 @@
 class Solver
+    def factorial(arg)
+        i = 1
+        result = 1
+        while i <= arg do            
+            result = i * result
+            i+=1
+        end 
+        result
+    end
 end
--- specs/solver_specs.rb
+++ specs/solver_specs.rb
@@ -3,7 +3,7 @@ require_relative '../solver'
 describe 'Factorial method TDD' do
   it 'Return the result of factorial' do
     solver = Solver.new
-    result = solver.factorial(5)
-    expect(result).to eq(120)
+    result = solver.factorial(4)
+    expect(result).to eq(24)
   end
 end
