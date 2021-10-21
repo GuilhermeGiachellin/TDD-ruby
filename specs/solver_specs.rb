4a8c31359be588f9aa7269758c47cded30d6ef0e
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-10-21 16:45:25 -0300

    Test in case argument is a negative number

--- specs/solver_specs.rb
+++ specs/solver_specs.rb
@@ -2,13 +2,15 @@ require_relative '../solver'
 
 describe 'Factorial method TDD' do
   solver = Solver.new
-  it 'Return the result of factorial' do    
-    result = solver.factorial(4)
-    expect(result).to eq(24)
+  it 'Return the result of factorial' do      
+    expect(solver.factorial(4)).to eq(24)
   end
 
-  it 'Return 1 if argument is 0' do
-    result = solver.factorial(0)
-    expect(result).to eq(1)
+  it 'Return 1 if argument is 0' do    
+    expect(solver.factorial(0)).to eq(1)
+  end  
+
+  it 'Accepts only 0 or positives' do
+    expect(solver.factorial(-2)).to eq('Negative numbers are not permited!')   
   end  
 end
