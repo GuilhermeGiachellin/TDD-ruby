2476af74813059da887e44c5381b131648d76164
arthurgc <alxguzmanc@gmail.com>
2021-10-21 15:11:45 -0500

    Fix indentation

--- solver.rb
+++ solver.rb
@@ -1,25 +1,24 @@
 class Solver
-    def factorial(arg)
-        if arg < 0 
-            return 'Negative numbers are not permited!'
-        end
-        i = 1
-        result = 1
-        while i <= arg do            
-            result = i * result
-            i+=1
-        end 
-        result
-    end
+  def factorial(arg)
+    return 'Negative numbers are not permited!' if arg.negative?
 
-    def reverse(string)
-        array_of_letters = string.split("")
-        array_word = []
-        index = array_of_letters.length - 1
-        while index >= 0 do        
-            array_word << array_of_letters[index]
-            index -= 1
-        end 
-       return array_word.join
+    i = 1
+    result = 1
+    while i <= arg
+      result = i * result
+      i += 1
     end
+    result
+  end
+
+  def reverse(string)
+    array_of_letters = string.chars
+    array_word = []
+    index = array_of_letters.length - 1
+    while index >= 0
+      array_word << array_of_letters[index]
+      index -= 1
+    end
+    array_word.join
+  end
 end
--- specs/solver_specs.rb
+++ specs/solver_specs.rb
@@ -2,31 +2,28 @@ require_relative '../solver'
 
 describe 'Factorial method TDD' do
   solver = Solver.new
-  it 'Return the result of factorial' do      
+  it 'Return the result of factorial' do
     expect(solver.factorial(4)).to eq(24)
   end
 
-  it 'Return 1 if argument is 0' do    
+  it 'Return 1 if argument is 0' do
     expect(solver.factorial(0)).to eq(1)
-  end  
+  end
 
   it 'Accepts only 0 or positives' do
-    expect(solver.factorial(-2)).to eq('Negative numbers are not permited!')   
-  end  
+    expect(solver.factorial(-2)).to eq('Negative numbers are not permited!')
+  end
 end
-
 
 describe 'reverse method TDD' do
   let(:solver) { Solver.new }
 
-  it "Call reverse method" do
+  it 'Call reverse method' do
     expect(solver).to receive(:reverse).with('Lorenzo')
     solver.reverse('Lorenzo')
   end
 
-  it "return string" do
+  it 'Return the reversed string' do
     expect(solver.reverse('Lorenzo')).to eq('ozneroL')
   end
-
-
 end
