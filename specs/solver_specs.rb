c2a1bf56b349d1c86a853c5e6eac5caeffc6730c
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-10-21 16:40:53 -0300

    Test in case argument is 0

--- specs/solver_specs.rb
+++ specs/solver_specs.rb
@@ -1,9 +1,14 @@
 require_relative '../solver'
 
 describe 'Factorial method TDD' do
-  it 'Return the result of factorial' do
-    solver = Solver.new
+  solver = Solver.new
+  it 'Return the result of factorial' do    
     result = solver.factorial(4)
     expect(result).to eq(24)
   end
+
+  it 'Return 1 if argument is 0' do
+    result = solver.factorial(0)
+    expect(result).to eq(1)
+  end  
 end
